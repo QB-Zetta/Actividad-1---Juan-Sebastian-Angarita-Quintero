9c3192c3f3f36a5a03f85626ba38271a439a5560
Seb <tucorreo@ejemplo.com>
2025-04-30 22:36:37 -0600

    Actividad 1 completa

--- activity/1_session/Actividad 1/index.js
+++ activity/1_session/Actividad 1/index.js
@@ -3,3 +3,36 @@
 import './aboutme.js';
 
 console.log("Ej 1");
+
+console.log("Ej 3 – Números divisibles por 7 del 1 al 100:");
+
+//Ejercicio 3 
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 7 === 0) {
+    console.log(i);
+  }
+}
+
+// Ejercicio 6
+
+// Objeto literal
+let formatter = {
+    prefix: "Hello ",
+    
+    // Método que imprime prefix + argumento
+    append: function(str) {
+      console.log(this.prefix + str);
+    }
+  };
+  // Probar metodo append
+  formatter.append("World"); // Result: Hello World
+  // Atributo prototype
+Object.setPrototypeOf(formatter, {
+    toLowerString: function(str) {
+      console.log(str.toLowerCase());
+    }
+  });
+  // Probar metodo toLowerString
+  formatter.toLowerString("I’m Sebas");   // Resultado: i’m Sebas
+  
